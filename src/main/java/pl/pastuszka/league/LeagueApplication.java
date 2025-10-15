aea29c237b8c5ff0a91976e96360137731a56917
Jakub Pastuszka <jaku6p@gmail.com>
2025-10-15 20:21:56 +0200

    Refactored code

--- src/main/java/pl/pastuszka/league/LeagueApplication.java
+++ src/main/java/pl/pastuszka/league/LeagueApplication.java
@@ -11,6 +11,7 @@ import pl.pastuszka.league.entity.League;
 import pl.pastuszka.league.entity.Team;
 import pl.pastuszka.league.utils.SerializeLeague;
 import pl.pastuszka.league.data.LeagueGenerator;
+import pl.pastuszka.league.utils.ThreadWriting;
 
 @SpringBootApplication
 public class LeagueApplication {
@@ -68,36 +69,8 @@ public class LeagueApplication {
 
 		System.out.println("=== Zadanie 7 ===");
 
-		ForkJoinPool customThreadPool = new ForkJoinPool(4);
-
-		final List<League> finalLeagues = leagues; 
-
-		try {
-			customThreadPool.submit(() ->{
-				finalLeagues.parallelStream().forEach(league -> {
-    				System.out.println("\n---" + Thread.currentThread().getName() + " Liga: " + league.getName() + " ---");
-
-    				league.getTeams().forEach(team -> {
-        				System.out.println("--- " + Thread.currentThread().getName() + " Druzyna: " + team.toString() + " ---");
-    				});
-
-    				//System.out.println("---" + Thread.currentThread().getName() + " Koniec ligi: " + league.getName() + " ---\n");
-				});
-			}).get();
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			// Zamknięcie puli wątków
-			customThreadPool.shutdown();
-			try {
-				if (!customThreadPool.awaitTermination(60, java.util.concurrent.TimeUnit.SECONDS)) {
-					customThreadPool.shutdownNow();
-				}
-			} catch (InterruptedException e) {
-				customThreadPool.shutdownNow();
-			}
-		}
+		ThreadWriting.parallelThread(leagues);
+		ThreadWriting.streamThread(leagues);
 	}
 
 }
